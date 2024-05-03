f55d03d2fa51b482e0ae59003894c97d36c0c717
fabio <fabio.calcinelli@tngtech.com>
2024-05-03 10:09:33 +0200

    inserted personal information

--- src/main.js
+++ src/main.js
@@ -4,25 +4,25 @@ new App({
 	target: document.body,
 	props: {
 		// What's your name?
-		name: 'James Butler',
+		name: 'Fabio Calcinelli',
 		// In the following fiels you can either give a single string, 
 		// or an array of bullet points
 		
 		// What do you associate with the term 'CI/CD'?
 		associations: [
-			'Ehm, what?'
+			'Fear of red pipelines'
 		],
 		// Which CI/CD tools do you use in your project?
-		tools: 'Jenkins, maybe?',
+		tools: 'GitLab CI/CD',
 		// What do you want to learn in this workshop? 
 		expectations: [
-			'what you are talking about'
+			'basic introduction to how software similar to GitLab CI/CD is intended to be used'
 		],
 		// What do you like to do when you're not coding?
 		hobbies: [
-			'play chess', 
-			'do skydiving', 
-			'do my taxes'
+			'lifting',
+			'reading',
+			'hiking'
 		]
 	}
 });
